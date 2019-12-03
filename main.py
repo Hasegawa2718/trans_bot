17fbaa6e9099fc5c12ebc8741fc459585d41d6a2
Hasegawa2718 <tosiki_801_life@outlook.jp>
2019-12-03 21:45:26 +0900

    make it better

--- main.py
+++ main.py
@@ -39,8 +39,7 @@ async def on_message(message):
                 embed.add_field(name='After', value=convert_string.text, inline=False)
                 await message.channel.send(embed=embed)
         else:
-            trans, str = list(say.split('='))
-            befor_lang, after_lang = list(trans.split('-'))
+            befor_lang, after_lang, str = list(say.split('-'))
             convert_string = translator.translate(str, src=befor_lang, dest=after_lang)
             embed = discord.Embed(title='変換結果', color=0xff0000)
             embed.add_field(name='Befor', value=str)
